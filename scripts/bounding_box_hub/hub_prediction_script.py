a04e6d577a8c78edfb8619ce50cf805e0e6fa3ab
marcus neo <81731201+marcus-datature@users.noreply.github.com>
2021-05-11 16:47:38 +0800

    updated the hub resource scripts

--- scripts/bounding_box_hub/hub_prediction_script.py
+++ scripts/bounding_box_hub/hub_prediction_script.py
@@ -17,35 +17,39 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 ## Comment out to set verbose to true
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
+PROJECT_SECRET="<YOUR_PROJECT_SECRET>"
+MODEL_KEY="<YOUR_MODEL_KEY>"
+INPUT_FOLDER = "./Test_Images/"
+OUTPUT_FOLDER = "./Outputs"
+THRESHOLD = 0.5
 
 def main():
     ## Load argument variables
 
     hub_model = HubModel(
-        project_secret="7b8f75e199a4a59b11371d5ee4c47d1e8d18a01f9d479c5be3dd3259cf61434b",
-        model_key="ded6a6636aa2e1cb1915b8976d32411c",
+        project_secret=PROJECT_SECRET,
+        model_key=MODEL_KEY,
     )
     trained_model = hub_model.load_tf_model()
     category_index = hub_model.load_label_map()
     ## Run prediction on each image
-    for each_image in glob.glob(os.path.join("./Test_Images/", "*")):
+    for each_image in glob.glob(os.path.join(INPUT_FOLDER, "*")):
         print("Predicting for {}...".format(each_image))
         input_tensor = hub_model.load_image_with_model_dimensions(each_image)
         ## Feed image into model
         detections_output = trained_model(input_tensor)
-
         visualized_image = visualize_bbox(
-            each_image, detections_output, category_index, 0.7
+            each_image, detections_output, category_index, THRESHOLD
         )
 
         ## Save predicted image
         filename = os.path.basename(each_image)  # each_image.split("/")[-1]
         image_predict = Image.fromarray(visualized_image)
-        image_predict.save(os.path.join("./Outputs", filename))
+        image_predict.save(os.path.join(OUTPUT_FOLDER, filename))
 
         print(
             "Saving predicted images to {}...".format(
-                os.path.join("./Outputs", filename)
+                os.path.join(OUTPUT_FOLDER, filename)
             )
         )
 
