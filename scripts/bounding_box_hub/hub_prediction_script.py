baff2135edeac4904f95dce2679eff2b14585c18
marcus neo <81731201+marcus-datature@users.noreply.github.com>
2021-05-11 17:22:39 +0800

    Cleaned Up Code

--- scripts/bounding_box_hub/hub_prediction_script.py
+++ scripts/bounding_box_hub/hub_prediction_script.py
@@ -1,20 +1,15 @@
-"""
-Prediction script for trained tensorflow PB model.
+"""Prediction script for trained tensorflow PB model.
 """
 
 import os
-import time
 import glob
-import numpy as np
-import tensorflow as tf
-import cv2
 from dataturehub.hub import HubModel
 from dataturehub.utils.visualize import visualize_bbox
 from PIL import Image
 
-## Comment out next line to use GPU
+# Comment out next line to use GPU
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-## Comment out to set verbose to true
+# Comment out to set verbose to true
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 PROJECT_SECRET="<YOUR_PROJECT_SECRET>"
@@ -24,7 +19,7 @@ OUTPUT_FOLDER = "./Outputs"
 THRESHOLD = 0.5
 
 def main():
-    ## Load argument variables
+    # Load argument variables
 
     hub_model = HubModel(
         project_secret=PROJECT_SECRET,
@@ -32,18 +27,18 @@ def main():
     )
     trained_model = hub_model.load_tf_model()
     category_index = hub_model.load_label_map()
-    ## Run prediction on each image
+    # Run prediction on each image
     for each_image in glob.glob(os.path.join(INPUT_FOLDER, "*")):
         print("Predicting for {}...".format(each_image))
         input_tensor = hub_model.load_image_with_model_dimensions(each_image)
-        ## Feed image into model
+        # Feed image into model
         detections_output = trained_model(input_tensor)
         visualized_image = visualize_bbox(
             each_image, detections_output, category_index, THRESHOLD
         )
 
-        ## Save predicted image
-        filename = os.path.basename(each_image)  # each_image.split("/")[-1]
+        # Save predicted image
+        filename = os.path.basename(each_image)
         image_predict = Image.fromarray(visualized_image)
         image_predict.save(os.path.join(OUTPUT_FOLDER, filename))
 
