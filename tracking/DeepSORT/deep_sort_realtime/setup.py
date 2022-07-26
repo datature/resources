b99ba8816698b2a860f9c8fc539827d8aa9a61e1
Keechin <1712699+gkeechin@users.noreply.github.com>
2022-07-26 17:20:44 +0800

    Initial Commit for Object Tracking Resources
    - Files Used for Demos
    - DeepSORT Modified Codes for Running Notebooks
    - To Fix Formatting and Attributions

---
+++ tracking/DeepSORT/deep_sort_realtime/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="deep-sort-realtime",
+    version="1.2",
+    author="levan92",
+    author_email="lingevan0208@gmail.com",
+    description="A more realtime adaptation of Deep SORT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/levan92/deep_sort_realtime",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(exclude=("test",)),
+    package_data={
+        "deep_sort_realtime.embedder": [
+            "weights/mobilenetv2_bottleneck_wts.pt",
+            "weights/download_clip_wts.sh",
+            "weights/download_tf_wts.sh",
+        ]
+    },
+    install_requires=[
+        "numpy",
+        "scipy",
+        "opencv-python",
+    ],
+)
